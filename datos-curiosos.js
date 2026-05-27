f8e2d8eb31e69b37d959260510e3531b73e261ae
pab99 <pab999@gmail.com>
2026-05-27 20:30:39 -0300

    Create datos-curiosos.js

---
+++ datos-curiosos.js
@@ -0,0 +1,23 @@
+// datos-curiosos.js
+const datosCuriosos = [
+    {
+        pregunta: "¿Quién fue el miembro más joven de la Primera Junta?",
+        explicacion: "¡Juan Larrea! Tenía solo 27 años cuando asumió como vocal el 25 de mayo de 1810."
+    },
+    {
+        pregunta: "¿El sol de la bandera ya existía en la Revolución de Mayo?",
+        explicacion: "No, el 'Sol de Mayo' se adoptó recién en 1813. Está inspirado en el dios del sol Inca, Inti."
+    },
+    {
+        pregunta: "¿Realmente se repartieron cintas celestes y blancas?",
+        explicacion: "Los historiadores afirman que en realidad eran cintas blancas (unión) y rojas (guerra), o retratos del rey Fernando VII."
+    },
+    {
+        pregunta: "¿Hizo buen tiempo aquel 25 de mayo?",
+        explicacion: "La tradición del 'Sol de Mayo' es literal: los registros confirman que el día estuvo lluvioso, frío y muy nublado."
+    },
+    {
+        pregunta: "¿Dónde se reunía en secreto la sociedad patriótica antes de la revolución?",
+        explicacion: "Se juntaban en la famosa Jabonería de Vieytes. Era el búnker conspirativo camuflado como negocio."
+    }
+];
